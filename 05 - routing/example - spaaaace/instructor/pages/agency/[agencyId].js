919e708980e9f634f683cc42359b82bc1d16c3f9
oa <oliver+github@abolish.golf>
2026-08-06 18:33:36 -0600

    rev [05 - routing]: ex01 - populating page with API data

--- 05 - routing/example - spaaaace/instructor/pages/agency/[agencyId].js
+++ 05 - routing/example - spaaaace/instructor/pages/agency/[agencyId].js
@@ -40,6 +40,7 @@ export default function Agency() {
         :
         <Container sx={{ paddingTop: 2 }}>
           <Grid container>
+
             <Grid item xs="2">
               <img
                 alt={agencyDetails.name}
@@ -49,9 +50,59 @@ export default function Agency() {
             </Grid>
             <Grid item xs="10">
               <Typography variant="h3" gutterBottom>
-                Agency Page for {agencyId}
+                {`${agencyDetails.name} (${agencyDetails.abbrev})`}
               </Typography>
             </Grid>
+
+            <Grid item xs="4">
+              <Typography variant="h5">
+                  {'Launch Details'}
+              </Typography>
+              <SimpleDetailsCard 
+                  title={'Total Launches'}
+                  description={agencyDetails.total_launch_count}
+              />
+              <SimpleDetailsCard 
+                  title={'Successful Launches'}
+                  description={agencyDetails.successful_launches}
+              />
+              <SimpleDetailsCard 
+                  title={'Successful Landings'}
+                  description={`${agencyDetails.successful_landings}`}
+              />
+            </Grid>
+
+            <Grid item xs="4">
+              <Typography variant="h5">
+                  {`Agency Information`}
+              </Typography>
+              <SimpleDetailsCard 
+                  title={'administrator'}
+                  description={`${agencyDetails.administrator}`}
+              />
+              <SimpleDetailsCard 
+                  title={'Space Agency Details'}
+                  description={`Founded ${agencyDetails.founding_year}`}
+                  subDescription={agencyDetails.description}
+              />
+            </Grid>
+
+            <Grid item xs="4">
+                <Typography variant="h5">
+                    {`SpaceCraft`}
+                </Typography>
+                { agencyDetails.spacecraft_list && agencyDetails.spacecraft_list.map((spaceCraft)=> {
+                    return <SimpleDetailsCard 
+                        key={spaceCraft.id}
+                        description={`${spaceCraft.name}`}
+                        buttonCallback={()=> {
+                          console.log("go to the space page")    
+                        }}
+                        buttonName="Go to SpaceCraft"
+                      />
+                })}
+            </Grid>       
+
           </Grid>
         </Container>
     }
